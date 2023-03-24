7a4767c1bacbeef18c5d0a8a49a55257ecbf65fd
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2023-03-24 15:39:34 -0400

    Commit 1: mock pages

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book.io</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h1>Welcome to Book.io</h1>
+		
+	</main>
+</body>
+</html>
