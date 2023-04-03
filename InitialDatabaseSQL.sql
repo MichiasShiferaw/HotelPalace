fcb852fc87f870bbcfbadc481185fd200846bb3a
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2023-04-03 09:47:38 -0400

    Add an trigger to verify if the address exists (#26)
    * Add an trigger to verify if the address exists

--- InitialDatabaseSQL.sql
+++ InitialDatabaseSQL.sql
@@ -27,13 +27,13 @@ PK = Address Info
 */
 CREATE TABLE IF NOT EXISTS address_info(
 	street_name VARCHAR(50) NOT NULL,
-    street_number SMALLINT NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    province VARCHAR(60) NOT NULL,
-    postal_code VARCHAR(6) NOT NULL,
-    country VARCHAR(20) NOT NULL,
+	street_number SMALLINT NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	province VARCHAR(60) NOT NULL,
+	postal_code VARCHAR(6) NOT NULL,
+	country VARCHAR(20) NOT NULL,
 
-    PRIMARY KEY (street_name, street_number, postal_code)
+	PRIMARY KEY (street_name, street_number, postal_code)
 );
 
 /*
@@ -44,16 +44,16 @@ FK = Address Info
 */
 CREATE TABLE IF NOT EXISTS hotel_chain (
 	chain_id VARCHAR(20) NOT NULL PRIMARY KEY,
-    chain_name VARCHAR(55)  NOT NULL,
-    street_name VARCHAR(50) NOT NULL,
-    street_number SMALLINT NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    province VARCHAR(60) NOT NULL,
-    postal_code VARCHAR(6) NOT NULL,
-    country VARCHAR(20) NOT NULL,
-    num_of_hotels VARCHAR(20)  NOT NULL,
-    phone_number VARCHAR(20)  NOT NULL,
-    email VARCHAR(50)  NOT NULL,
+	chain_name VARCHAR(55)  NOT NULL,
+	street_name VARCHAR(50) NOT NULL,
+	street_number SMALLINT NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	province VARCHAR(60) NOT NULL,
+	postal_code VARCHAR(6) NOT NULL,
+	country VARCHAR(20) NOT NULL,
+	num_of_hotels VARCHAR(20)  NOT NULL,
+	phone_number VARCHAR(20)  NOT NULL,
+	email VARCHAR(50)  NOT NULL,
     
 	FOREIGN KEY (street_name, street_number, postal_code) REFERENCES address_info(street_name, street_number, postal_code)
 );
@@ -66,24 +66,26 @@ FK = chain_id ,Address Info
 
 */
 
+-- CREATE TYPE rating AS ENUM('1','2','3','4','5'); 
+
 CREATE TABLE IF NOT EXISTS hotel (
 	chain_id VARCHAR(20)  NOT NULL,
-    hotel_id INT NOT NULL UNIQUE,
-    hotel_name VARCHAR(20)  NOT NULL,
-    street_name VARCHAR(50) NOT NULL,
-    street_number SMALLINT NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    province VARCHAR(60) NOT NULL,
-    postal_code VARCHAR(6) NOT NULL,
-    country VARCHAR(20) NOT NULL,
+    	hotel_id INT NOT NULL UNIQUE,
+    	hotel_name VARCHAR(100)  NOT NULL,
+    	street_name VARCHAR(50) NOT NULL,
+    	street_number SMALLINT NOT NULL,
+    	city VARCHAR(20) NOT NULL,
+    	province VARCHAR(60) NOT NULL,
+    	postal_code VARCHAR(6) NOT NULL,
+    	country VARCHAR(20) NOT NULL,
 	-- manager_SSN VARCHAR(20) , 
-    num_of_rooms INT  NOT NULL,
-    rating enum ('1','2','3','4','5')  NOT NULL,
-    email VARCHAR(50)  NOT NULL,
+    	num_of_rooms INT  NOT NULL,
+    	rating INT NOT NULL check(rating >=1 and rating<=5),
+    	email VARCHAR(50)  NOT NULL,
     
-    PRIMARY KEY(chain_id,hotel_id),
+    	PRIMARY KEY(chain_id,hotel_id),
     
-    FOREIGN KEY (chain_id) REFERENCES hotel_chain(chain_id),
+    	FOREIGN KEY (chain_id) REFERENCES hotel_chain(chain_id),
 	FOREIGN KEY (street_name, street_number, postal_code) REFERENCES address_info(street_name, street_number, postal_code)
 
 );
@@ -98,12 +100,12 @@ FK = hotel_id
 */
 CREATE TABLE IF NOT EXISTS hotel_phone(
 	hotel_id INT NOT NULL,
-    phone_number VARCHAR(15)  NOT NULL unique,
-    department VARCHAR(20)  NOT NULL,
+    	phone_number VARCHAR(15)  NOT NULL unique,
+    	department VARCHAR(20)  NOT NULL,
     
-    PRIMARY KEY(hotel_id,phone_number),
+    	PRIMARY KEY(hotel_id,phone_number),
     
-    FOREIGN KEY(hotel_id) REFERENCES hotel(hotel_id)
+    	FOREIGN KEY(hotel_id) REFERENCES hotel(hotel_id)
 
 );
 
@@ -115,24 +117,24 @@ FK = chain_id, hotel_id, Address Info
 */
 CREATE TABLE IF NOT EXISTS employee(
 	emp_SSN VARCHAR(20) NOT NULL PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    middle_name VARCHAR(20) ,
-    last_name VARCHAR(20) NOT NULL,
+    	first_name VARCHAR(20) NOT NULL,
+    	middle_name VARCHAR(20) ,
+    	last_name VARCHAR(20) NOT NULL,
     
-    street_name VARCHAR(50) NOT NULL,
-    street_number SMALLINT NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    province VARCHAR(60) NOT NULL,
-    postal_code VARCHAR(6) NOT NULL,
-    country VARCHAR(20) NOT NULL,
+    	street_name VARCHAR(50) NOT NULL,
+    	street_number SMALLINT NOT NULL,
+    	city VARCHAR(20) NOT NULL,
+    	province VARCHAR(60) NOT NULL,
+    	postal_code VARCHAR(6) NOT NULL,
+    	country VARCHAR(20) NOT NULL,
     
 	chain_id VARCHAR(20)  NOT NULL,
-    hotel_id INT NOT NULL,
-    start_date DATE NOT NULL,
-    password VARCHAR(20),
-    last_update DATE DEFAULT (CURRENT_DATE),
+    	hotel_id INT NOT NULL,
+    	start_date DATE NOT NULL,
+    	password VARCHAR(20),
+   	last_update DATE DEFAULT (CURRENT_DATE),
 	
-    Foreign Key (chain_id,hotel_id) REFERENCES hotel(chain_id, hotel_id),
+   	FOREIGN KEY(chain_id,hotel_id) REFERENCES hotel(chain_id, hotel_id),
 	FOREIGN KEY (street_name, street_number, postal_code) REFERENCES address_info(street_name, street_number, postal_code)
     
 );
@@ -145,13 +147,13 @@ FK = emp_SSN
 */
 CREATE TABLE IF NOT EXISTS role(
 	emp_SSN VARCHAR(20) NOT NULL,
-    role_id VARCHAR(20) NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    salary DECIMAL(6,2) NOT NULL,
+    	role_id VARCHAR(20) NOT NULL,
+    	name VARCHAR(20) NOT NULL,
+    	salary DECIMAL(6,2) NOT NULL,
     
-    Primary Key(emp_SSN, role_id),
+    	PRIMARY KEY(emp_SSN, role_id),
     
-    FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN)
+    	FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN)
 );
 
 /*
@@ -162,12 +164,12 @@ FK = chain_id, hotel_id, manager_SSN
 */
 CREATE TABLE IF NOT EXISTS hotel_management (
 	chain_id VARCHAR(20) NOT NULL,
-    hotel_id INT NOT NULL UNIQUE,
-    manager_SSN VARCHAR(20) NOT NULL UNIQUE, 
-    Foreign Key (chain_id,hotel_id) REFERENCES hotel(chain_id, hotel_id),
-    FOREIGN KEY(manager_SSN) references employee(emp_SSN),
+    	hotel_id INT NOT NULL UNIQUE,
+    	manager_SSN VARCHAR(20) NOT NULL UNIQUE, 
+    	Foreign Key (chain_id,hotel_id) REFERENCES hotel(chain_id, hotel_id),
+    	FOREIGN KEY(manager_SSN) references employee(emp_SSN),
     
-    Primary Key (chain_id, hotel_id, manager_SSN)
+    	PRIMARY KEY (chain_id, hotel_id, manager_SSN)
 
 );
 
@@ -180,20 +182,20 @@ FK = Address Info
 */
 CREATE TABLE IF NOT EXISTS customer(
 	customer_SSN VARCHAR(20)  NOT NULL PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    middle_name VARCHAR(20) ,
-    last_name VARCHAR(20) NOT NULL,
-    email VARCHAR(50)  NOT NULL,
-    street_name VARCHAR(50) NOT NULL,
-    street_number SMALLINT NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    province VARCHAR(60) NOT NULL,
-    postal_code VARCHAR(6) NOT NULL,
-    country VARCHAR(20) NOT NULL,
-    joining_date DATE NOT NULL,
-    phone_number VARCHAR(20)  NOT NULL UNIQUE,
-    password VARCHAR(20)  NOT NULL,
-    last_updated DATE DEFAULT (CURRENT_DATE),
+	first_name VARCHAR(20) NOT NULL,
+	middle_name VARCHAR(20) ,
+	last_name VARCHAR(20) NOT NULL,
+	email VARCHAR(50)  NOT NULL,
+	street_name VARCHAR(50) NOT NULL,
+	street_number SMALLINT NOT NULL,
+	city VARCHAR(20) NOT NULL,
+	province VARCHAR(60) NOT NULL,
+	postal_code VARCHAR(6) NOT NULL,
+	country VARCHAR(20) NOT NULL,
+	joining_date DATE NOT NULL,
+	phone_number VARCHAR(20)  NOT NULL UNIQUE,
+	password VARCHAR(20)  NOT NULL,
+	last_updated DATE DEFAULT (CURRENT_DATE),
     
 	FOREIGN KEY (street_name, street_number, postal_code) REFERENCES address_info(street_name, street_number, postal_code)
 
@@ -204,11 +206,14 @@ Room Category Table
 PK = room_category_id
 
 */
+
+CREATE TYPE capacity AS ENUM('single', 'double', 'deluxe', 'suite');
+CREATE TYPE views AS ENUM('mountain', 'sea');
 CREATE TABLE IF NOT EXISTS room_category(
 	room_category_id VARCHAR(4)  NOT NULL PRIMARY KEY,
-    capacity enum ('single', 'double', 'deluxe', 'suite')  NOT NULL,
-    view enum ('mountain', 'sea')  NOT NULL,
-    is_extendable BOOLEAN  NOT NULL
+	size capacity NOT NULL,
+	scenary views NOT NULL,
+	is_extendable BOOLEAN  NOT NULL
     
 );
 
@@ -220,17 +225,17 @@ FK = hotel_id, room_category_id
 */
 CREATE TABLE IF NOT EXISTS room(
 	room_no VARCHAR(20) NOT NULL,
-    hotel_id INT NOT NULL,
-    price DECIMAL(5,2) NOT NULL,
-    room_category_id VARCHAR(20) NOT NULL,
-    amenities VARCHAR(50)  NOT NULL,
-    damages VARCHAR(50)  NOT NULL,
-    last_updated DATE DEFAULT (CURRENT_DATE),
-    
-    PRIMARY KEY(hotel_id,room_no),
-    
-    FOREIGN KEY(hotel_id) REFERENCES hotel(hotel_id),
-    FOREIGN KEY(room_category_id) REFERENCES room_category(room_category_id)
+	hotel_id INT NOT NULL,
+	price DECIMAL(5,2) NOT NULL,
+	room_category_id VARCHAR(20) NOT NULL,
+	amenities VARCHAR(50)  NOT NULL,
+	damages VARCHAR(50)  NOT NULL,
+	last_updated DATE DEFAULT (CURRENT_DATE),
+
+	PRIMARY KEY(hotel_id,room_no),
+
+	FOREIGN KEY(hotel_id) REFERENCES hotel(hotel_id),
+	FOREIGN KEY(room_category_id) REFERENCES room_category(room_category_id)
 );
 
 /*
@@ -239,24 +244,27 @@ PK = booking_id, hotel_id, customer_SSN, room_no, emp_SSN
 FK = emp_ssn, customer_ssn, hotel_id, room_no
 
 */
+
+CREATE TYPE booking_status AS ENUM('start', 'completed', 'cancel','archive'); 
+
 CREATE TABLE IF NOT EXISTS booking_info(
 
 	booking_id VARCHAR(20)  NOT NULL,
-    hotel_id INT NOT NULL,
-    customer_SSN VARCHAR(20)  NOT NULL,
-    status enum('start', 'completed', 'cancel','archive')  NOT NULL,
-    room_no VARCHAR(20)  NOT NULL,
-    emp_SSN VARCHAR(20)  NOT NULL,
-    arrival_time DATE  NOT NULL,
-    departure_time DATE  NOT NULL,
-    created_at DATE  NOT NULL,
-    last_updated DATE DEFAULT (CURRENT_DATE),
-    
-    PRIMARY KEY(booking_id, hotel_id, customer_SSN, room_no, emp_SSN),
-    
-    FOREIGN KEY(customer_SSN) REFERENCES customer(customer_SSN),
-    FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN),
-    FOREIGN KEY(hotel_id,room_no) REFERENCES room(hotel_id,room_no)
+	hotel_id INT NOT NULL,
+	customer_SSN VARCHAR(20)  NOT NULL,
+	status booking_status NOT NULL,
+	room_no VARCHAR(20)  NOT NULL,
+	emp_SSN VARCHAR(20)  NOT NULL,
+	arrival_time DATE  NOT NULL,
+	departure_time DATE  NOT NULL,
+	created_at DATE  NOT NULL,
+	last_updated DATE DEFAULT (CURRENT_DATE),
+
+	PRIMARY KEY(booking_id, hotel_id, customer_SSN, room_no, emp_SSN),
+
+	FOREIGN KEY(customer_SSN) REFERENCES customer(customer_SSN),
+	FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN),
+	FOREIGN KEY(hotel_id,room_no) REFERENCES room(hotel_id,room_no)
 
 
 );
@@ -267,26 +275,69 @@ PK = renting_id, hotel_id, customer_SSN, emp_SSN, room_no
 FK = emp_ssn, customer_ssn, hotel_id, room_no
 
 */
+
+CREATE TYPE renting_status AS ENUM ('renting', 'checked-out', 'archive'); 
+
 CREATE TABLE IF NOT EXISTS renting_info(
 
 	renting_id VARCHAR(20) NOT NULL,
-    hotel_id INT NOT NULL,
-    status enum('renting', 'checked-out', 'archive') NOT NULL,
-    customer_SSN VARCHAR(20)  NOT NULL,
-    emp_SSN VARCHAR(20) NOT NULL,
-    room_no VARCHAR(20) NOT NULL,
-    booking_id VARCHAR(20) NOT NULL,
-    has_booked VARCHAR(20) NOT NULL,
-    arrival_time DATE NOT NULL,
-    departure_time DATE NOT NULL,
-    created_at DATE NOT NULL,
-    last_updated DATE DEFAULT (CURRENT_DATE),
-    
-    PRIMARY KEY(renting_id, hotel_id, customer_SSN, emp_SSN, room_no),
+	hotel_id INT NOT NULL,
+	status renting_status  NOT NULL,
+	customer_SSN VARCHAR(20)  NOT NULL,
+	emp_SSN VARCHAR(20) NOT NULL,
+	room_no VARCHAR(20) NOT NULL,
+	booking_id VARCHAR(20) NOT NULL,
+	has_booked VARCHAR(20) NOT NULL,
+	arrival_time DATE NOT NULL,
+	departure_time DATE NOT NULL,
+	created_at DATE NOT NULL,
+	last_updated DATE DEFAULT (CURRENT_DATE),
 
-    FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN),
-    -- FOREIGN KEY(booking_id) REFERENCES booking_info(booking_id),
-    FOREIGN KEY(customer_SSN) REFERENCES customer(customer_SSN),
-    FOREIGN KEY(hotel_id,room_no) REFERENCES room(hotel_id,room_no)
+	PRIMARY KEY(renting_id, hotel_id, customer_SSN, emp_SSN, room_no),
+
+	FOREIGN KEY(emp_SSN) REFERENCES employee(emp_SSN),
+	-- FOREIGN KEY(booking_id) REFERENCES booking_info(booking_id),
+	FOREIGN KEY(customer_SSN) REFERENCES customer(customer_SSN),
+	FOREIGN KEY(hotel_id,room_no) REFERENCES room(hotel_id,room_no)
 
 );
+
+
+-- idempotent
+CREATE OR REPLACE FUNCTION duplicate_addy_insert() RETURNS TRIGGER AS
+  $BODY$
+  BEGIN
+  IF NOT EXISTS(SELECT 1 FROM address_info WHERE street_name = new.street_name AND street_number = new.street_number AND postal_code = new.postal_code) THEN
+  INSERT INTO address_info(street_number, street_name, city, province, postal_code, country)
+  VALUES (new.street_number, new.street_name, new.postal_code, new.city, new.province_state, new.country);
+  END IF;
+  RETURN new;
+  END;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
+CREATE TRIGGER employee_addy
+  BEFORE UPDATE
+  ON employee
+  FOR EACH ROW
+  EXECUTE PROCEDURE duplicate_addy_insert();
+
+CREATE TRIGGER customer_addy
+  BEFORE UPDATE
+  ON customer
+  FOR EACH ROW
+  EXECUTE PROCEDURE duplicate_addy_insert();
+
+CREATE TRIGGER hotel_chain_addy
+  BEFORE UPDATE
+  ON hotel_chain
+  FOR EACH ROW
+  EXECUTE PROCEDURE duplicate_addy_insert();
+
+CREATE TRIGGER hotel_addy
+  BEFORE UPDATE
+  ON hotel
+  FOR EACH ROW
+  EXECUTE PROCEDURE duplicate_addy_insert();
