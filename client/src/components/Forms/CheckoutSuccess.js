6090b6e70208aa34b7d913d0e83013ac5d350080
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2023-04-11 11:18:15 -0400

    ReCommit with working checkmark

--- client/src/components/Forms/CheckoutSuccess.js
+++ client/src/components/Forms/CheckoutSuccess.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import {StyleSheet, Text, View} from 'react';
+import { Checkmark } from 'react-checkmark';
 
 const CheckoutSuccess = () => {
     return (
-        <div>
+        <>
+        <br></br>
+        <Checkmark size='250px'/>
+
+
+        <div style={{
+            textAlign: 'center',
+        }}>
             <h1>Thank you for your purchase!</h1>
             <h2>Order Number: 123456789</h2>
             <h2>Order Total: $100.00</h2>
         </div>
+        
+        </>
+        
     );
 }
+
 
 export default CheckoutSuccess;
