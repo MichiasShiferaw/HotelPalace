bda258a7276865f20a9842b2d443048c8bcc6301
MichiasShiferaw <mshif073@uottawa.ca>
2023-05-05 19:35:17 -0400

    Add new logo for homepage

--- client/src/pages/MainLandingPage.js
+++ client/src/pages/MainLandingPage.js
@@ -6,7 +6,10 @@ const MainLandingPage = () => {
     <>
       <div
         className="container-fluid cover d-flex text-center "
-        style={{ background: "linear-gradient(120deg,#ff2506 0%,#ff705f 60%,#ff705f 60%,#ffcb66 60%,#ffae11)" }}
+        style={{
+          background:
+            "linear-gradient(120deg,#ff2506 0%,#ff705f 60%,#ff705f 60%,#ffcb66 60%,#ffae11)",
+        }}
       >
         <div className="cover-container d-flex w-100 h-100 p-3 py-4 mx-auto flex-column">
           <header className="mb-auto">
@@ -16,18 +19,22 @@ const MainLandingPage = () => {
           </header>
           <div className="container my-5">
             <div className="p-5 text-center bg-body-tertiary rounded-3">
-              <svg
-                className="bi mt-4 mb-3"
-                style={{ color: "var(--bs-indigo)" }}
-                width="100"
+              <img
+                class="d-block mx-auto mb-4"
+                src="/assets/images/logo/logo1.png"
+                alt=""
+                width="126"
                 height="100"
-              ></svg>
-              <h1 className="text-body-emphasis pb-4">Welcome to Hotel Palace</h1>
+              ></img>
+              <h1 className="text-body-emphasis pb-4">
+                Welcome to Hotel Palace
+              </h1>
               <div className="d-inline-flex gap-2 mb-5">
                 <Link to="/cust/home">
                   <button
-                    className="d-inline-flex align-items-center fw-bold border-secondary btn btn-primary btn-lg px-4"
+                    className="d-inline-flex align-items-center fw-bold border-secondary btn btn-lg px-4 text-light"
                     type="button"
+                    style={{ backgroundColor: "#4D8B31" }}
                   >
                     View Customer Experience
                   </button>
